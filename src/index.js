4feb04039ca6b71614aad8f346fb906a2a23ca4c
alanps <alanps3699@gmail.com>
2026-02-24 12:26:34 +0530

    added frontend url to avoid cors with https

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:'mobilesoiltestinglabtcr.netlify.app',credentials:true}));
+app.use(cors({origin:'https://mobilesoiltestinglabtcr.netlify.app',credentials:true}));
 app.use('/api/auth',authRoute);
 app.use('/api/test',testRoute);
 app.use('/api/plot',plotRoute);
